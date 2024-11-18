4542735cc3f1e1cdbba5a9a3eb1169d28c9136a4
earl-stephens <stephensearl@hotmail.com>
2024-11-17 18:38:33 -0700

    Use registerBean

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -2,20 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main;
+package config;
+
+import org.springframework.context.annotation.Configuration;
 
 /**
  *
  * @author earltstephens
  */
-public class Parrot {
-    private String name;
+@Configuration
+public class ProjectConfig {
     
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
 }
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import config.ProjectConfig;
+import java.util.function.Supplier;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ *
+ * @author earltstephens
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        
+        Parrot x = new Parrot();
+        x.setName("Kiki");
+        
+        Supplier<Parrot> parrotSupplier = () -> x;
+        
+        context.registerBean("parrot1", 
+                Parrot.class, 
+                parrotSupplier,
+                bc -> bc.setPrimary(true));
+        
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+    }
+}
