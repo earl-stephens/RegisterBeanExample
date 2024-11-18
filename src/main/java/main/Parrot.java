8663fa56156e0f481d78d7b3fca6272df4aefe48
earl-stephens <stephensearl@hotmail.com>
2024-11-17 18:07:35 -0700

    Initial commit

---
+++ src/main/java/main/Parrot.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author earltstephens
+ */
+public class Parrot {
+    private String name;
+    
+    public String getName(String name) {
+        return name;
+    }
+    
+    public void setName() {
+        this.name = name;
+    }
+}
---
+++ src/main/java/main/RegisterBeanExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author earltstephens
+ */
+public class RegisterBeanExample {
+
+    public static void main(String[] args) {
+        
+    }
+}
